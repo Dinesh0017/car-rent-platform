0cb6807a4b89489aa969fa5e25669cbc787af3d4
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-04-06 12:41:24 +0530

    change hero section image path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import heroCar from "../assets/heroCar.jpg"; // Ensure the image is inside /assets folder
+import heroCar from "../public/assets/heroCar.jpg"; // Ensure the image is inside /assets folder
 import Button from "./Button";
 
 const HeroSection = () => {
