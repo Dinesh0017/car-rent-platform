440646f4dc9101b1db806afe2b6b4b43263371f0
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-04-10 12:59:33 +0530

    about page build

---
+++ src/components/JoinMember.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const JoinMember = () => {
+  return (
+    <section className="mt-20">
+      <h3 className="text-2xl font-bold text-blue-800">Join Our Team</h3>
+      <p className="text-gray-600 mt-2 mb-4">
+        Are you passionate about the car rental industry? Join our team and be
+        part of a dynamic and customer-focused company.
+      </p>
+      <button className="px-6 py-2 border border-blue-600 text-blue-600 rounded hover:bg-blue-100">
+        View Job Openings
+      </button>
+    </section>
+  );
+};
+
+export default JoinMember;
